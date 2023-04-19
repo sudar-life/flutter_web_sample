8bfdfa8301d90da3ca463caf7dcec1d32ee33c22
Bomi Kang <128353585+tc-kbm@users.noreply.github.com>
2023-04-19 15:05:14 +0900

    content item 변경
    type style 테스트를 위한 한글 입숨 더미 텍스트 추가

--- lib/src/pages/root_page.dart
+++ lib/src/pages/root_page.dart
@@ -12,12 +12,12 @@ class RootPage extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 15),
             child: Text(
-              'Section 1.10.32 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC',
+              '청춘을 할지라도 주는 무엇을 같은 수 너의 부패를 쓸쓸하랴',
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
             ),
           ),
           Text(
-            '"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"',
+            '가슴이 할지니, 이것을 말이다. 오직 품었기 사라지지 방지하는 사람은 위하여서, 품고 예가 것이다. 뜨고, 천지는 곳이 이상 같은 같이 풀이 창공에 무엇을 그리하였는가? 대중을 따뜻한 위하여 위하여서. 그러므로 우리의 두기 피가 위하여 힘있다. 보배를 어디 있는 할지라도 원대하고, 부패를 청춘의 칼이다. 가슴이 가슴에 쓸쓸한 이 붙잡아 황금시대다. 얼마나 따뜻한 위하여 안고, 들어 것이다. 피어나기 가진 청춘은 청춘의 두손을 보는 밥을 청춘의 사는가 약동하다.',
             style: TextStyle(fontSize: 14),
           )
         ],
